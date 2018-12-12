bf73b1eb2d49c31472845ac40c86f95fe4285ec6
Carson Coley <carsoncoley@cs.ucsb.edu>
2018-12-11 17:35:39 -0800

    Updated and Completed trial 1

--- recLinkedListFuncs.cpp
+++ recLinkedListFuncs.cpp
@@ -6,13 +6,26 @@
 //return sum of all values in linked list using a recursive approach
 //if head is null return 0
 int recursiveSum(Node* head) {
-  return -42;
+  if(!head){
+	  return 0;
+  }
+  return recursiveSum(head->next) + head->data;
 }
 
 //head: ptr to a Node* which is the head of a linked list
 //return the largest value in the linked list using a recursive approach
 //you may assume the list has at least one element
 int recursiveLargestValue(Node* head) {
-
-  return -42;
+  if(!head){
+	  return 0;
+  }
+  if(!head->next){
+	  return head->data;
+  }
+  int max = head->data;
+  int next = recursiveLargestValue(head->next);
+  if(next > max){
+	  return next;
+  }
+  return max;
 }
